72c5cc1b39da6263033067468d7b74fff0f4a0e7
ShobhitTyagiiii <155055575+ShobhitTyagiiii@users.noreply.github.com>
2025-07-11 17:02:27 +0530

    Prepare project for Railway deployment

--- ExpenseTracker/settings.py
+++ ExpenseTracker/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 # Base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,10 +58,7 @@ WSGI_APPLICATION = 'ExpenseTracker.wsgi.application'
 
 # Database (SQLite for offline development)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 # Password validation
@@ -77,10 +76,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'Tracker' / 'static',
-]
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
